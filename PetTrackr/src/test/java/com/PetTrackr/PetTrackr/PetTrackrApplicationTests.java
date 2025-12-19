34ce312385d3a85d039a81006aed2d6be264a869
Miguel M Ocque <142752734+miguelocque@users.noreply.github.com>
2025-12-19 15:34:16 -0500

    Initial Spring Initializr setup

---
+++ PetTrackr/src/test/java/com/PetTrackr/PetTrackr/PetTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PetTrackr.PetTrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
