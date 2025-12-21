71b10a6e221a95440fa4d799b8eb65c6e7ed0653
Miguel Ocque <mocque@bu.edu>
2025-12-20 19:05:12 -0500

    Created two query methods based on use cases for Feeding schedules; one retrieves individual pet schedules, and the other retrieves all the pet schedules by their owner id

---
+++ PetTrackr/src/main/java/com/PetTrackr/PetTrackr/repository/FeedingScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.PetTrackr.PetTrackr.repository;
+
+import com.PetTrackr.PetTrackr.entity.FeedingSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface FeedingScheduleRepository extends JpaRepository<FeedingSchedule, Long> {
+    // for individual pet feeding schedules
+    List<FeedingSchedule> findByPetId(Long petId);
+
+    // for all feeding schedules
+    List<FeedingSchedule> findAllFeedingSchedules();
+}
